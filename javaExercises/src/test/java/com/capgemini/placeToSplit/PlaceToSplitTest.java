7251d0da53cb73cf5f6789a4966842b8e0c528c3
KGALKA <KGALKA@CE03275.corp.capgemini.com>
2015-07-13 16:39:49 +0200

    Added tests (from comments) to PlaceToSplit

--- javaExercises/src/test/java/com/capgemini/placeToSplit/PlaceToSplitTest.java
+++ javaExercises/src/test/java/com/capgemini/placeToSplit/PlaceToSplitTest.java
@@ -29,10 +29,28 @@ public class PlaceToSplitTest {
 		int[] array = { 1, 1, 7 };
 		assertFalse(PlaceToSplit.canBalance(array));
 	}
-	
+
 	@Test
 	public void shouldReturnTrueFor1_1_2_5_5_4() {
 		int[] array = { 1, 1, 2, 5, 5, 4 };
+		assertTrue(PlaceToSplit.canBalance(array));
+	}
+
+	@Test
+	public void shouldReturnTrueFor1_1_1_2_1() {
+		int[] array = { 1, 1, 1, 2, 1 };
+		assertTrue(PlaceToSplit.canBalance(array));
+	}
+
+	@Test
+	public void shouldReturnFalseFor2_1_1_2_1() {
+		int[] array = { 2, 1, 1, 2, 1 };
+		assertFalse(PlaceToSplit.canBalance(array));
+	}
+
+	@Test
+	public void shouldReturnTrueFor10_10() {
+		int[] array = { 10, 10 };
 		assertTrue(PlaceToSplit.canBalance(array));
 	}
 
